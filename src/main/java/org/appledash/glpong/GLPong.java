4005e07f3e2ed0f91d911fcae3aa016e1897d8f6
AppleDash <derp@derpy.fr>
2017-07-05 20:56:59 -0400

    Add FPS counter

--- src/main/java/org/appledash/glpong/GLPong.java
+++ src/main/java/org/appledash/glpong/GLPong.java
@@ -10,7 +10,9 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.TrueTypeFont;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -27,6 +29,13 @@ public class GLPong {
     private Ball ball;
     private Paddle[] paddles = new Paddle[2];
 
+    private TrueTypeFont trueTypeFont;
+    private long framesRendered;
+    private double startTime;
+    private double timePassed;
+    private float fps;
+    private double fpsStartTime;
+
     public GLPong(DisplayMode displayMode) {
         try {
             this.setupDisplay(displayMode);
@@ -37,6 +46,10 @@ public class GLPong {
         this.ball = new Ball(new Vec3(Display.getWidth() / 2, Display.getHeight() / 2));
         this.paddles[LEFT] = new Paddle(new Vec3(50, Display.getHeight() / 2));
         this.paddles[RIGHT] = new Paddle(new Vec3(Display.getWidth() - 50, Display.getHeight() / 2));
+
+        trueTypeFont = new TrueTypeFont(new Font("Verdana", Font.PLAIN, 16), true);
+
+        this.startTime = System.currentTimeMillis() / 1000;
     }
 
     private void setupDisplay(DisplayMode displayMode) throws LWJGLException {
@@ -65,6 +78,7 @@ public class GLPong {
         long prevTime = System.currentTimeMillis();
         while (!Display.isCloseRequested()) {
             double deltaTime = (System.currentTimeMillis() - prevTime) / 1000D;
+            this.timePassed += deltaTime;
             prevTime = System.currentTimeMillis();
             this.draw();
 
@@ -119,6 +133,28 @@ public class GLPong {
         }
 
         ball.draw();
+
+        double curTime = System.currentTimeMillis() / 1000;
+        this.timePassed = curTime - this.startTime;
+
+        if ((this.timePassed - this.fpsStartTime) > 0.25 && this.framesRendered > 10) {
+            this.fps = (float) (this.framesRendered / (this.timePassed - this.fpsStartTime));
+            this.fpsStartTime = this.timePassed;
+            this.framesRendered = 0;
+        }
+        this.drawString(2, 2, "Frames: " + this.fps);
+
+        this.framesRendered++;
+    }
+
+    private void drawString(int x, int y, String str) {
+        GL11.glPushMatrix();
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        this.trueTypeFont.drawString(x, y, str);
+        this.trueTypeFont.drawString(x + 1, y + 1, str);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glPopMatrix();
     }
 
     public Paddle[] getPaddles() {
